dc493e9a215a93b509f4a8872a5b3f88303feb85
Md Hasibur Rahman <hasib@MddeMacBook-Pro.local>
2025-09-07 05:03:49 +0800

    loaded news details

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const loadCategory = () => {
  */
 
 const categoryContainer = document.getElementById("category-container");
+const newsContainer = document.getElementById("newsContainer");
 
 const loadCategoryAsync = async () => {
   try {
@@ -24,8 +25,6 @@ const loadCategoryAsync = async () => {
     console.log(error);
   }
 };
-
-loadCategoryAsync();
 
 const showCategory = (categories) => {
   categories.forEach((category) => {
@@ -42,6 +41,41 @@ const showCategory = (categories) => {
     });
     if (e.target.localName === "li") {
       e.target.classList.add("border-b-4");
+      loadNewsByCategory(e.target.id);
     }
   });
 };
+
+const loadNewsByCategory = async (categoryId) => {
+  try {
+    const res = await fetch(
+      `https://news-api-fs.vercel.app/api/categories/${categoryId}`
+    );
+    const data = await res.json();
+    const articles = data.articles;
+    showNewsByCategory(articles);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const showNewsByCategory = (articles) => {
+  console.log(articles);
+  newsContainer.innerHTML = "";
+  articles.forEach((article) => {
+    newsContainer.innerHTML += `
+        <div class = "border-2 border-gray-100 rounded-lg"> 
+            <div>
+             <img src='${article.image.srcset[5].url}'> 
+            </div>
+            <div class = "p-2">
+                <h1 class = "font-extrabold mt-3"> ${article.title}</h1>
+                <p class = "mt-3 text-sm"> ${article.time}</p>
+            </div>
+        </div>
+        `;
+  });
+};
+
+loadCategoryAsync();
+loadNewsByCategory("main");
